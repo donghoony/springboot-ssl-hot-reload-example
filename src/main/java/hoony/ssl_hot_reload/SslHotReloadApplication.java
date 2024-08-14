2219ddb8e8737a93639de066b2060d6e65a2b96a
donghoony <aru0504@naver.com>
2024-08-14 16:26:56 +0900

    init: Initialize project

---
+++ src/main/java/hoony/ssl_hot_reload/SslHotReloadApplication.java
@@ -0,0 +1,13 @@
+package hoony.ssl_hot_reload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslHotReloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SslHotReloadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hoony/ssl_hot_reload/SslHotReloadApplicationTests.java
@@ -0,0 +1,13 @@
+package hoony.ssl_hot_reload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SslHotReloadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
